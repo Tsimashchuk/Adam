ade0a87013204c46bdaca739f3f837b0dee05638
Adam <adama.timosuk@gmail.com>
2024-06-14 00:45:13 +0300

    add src

--- src/masks.py
+++ src/masks.py
@@ -1,31 +1,20 @@
-from typing import Any
-
-
-def get_mask_card_number(card: str) -> Any:
+def get_mask_card_number(card: str) -> str:
     """Функция, маскирнющая карты"""
-    card_number = card.split()[-1]
+    card = str(card)
+    new_card = card[:4] + " " + card[4:6] +  "**" + " " + "****" + " " + card[:4]
 
-    private_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
-
-    chunks, chunk_size = len(private_number), len(private_number) // 4
-    return " ".join(
-        [private_number[i: i + chunk_size] for i in range(0, chunks, chunk_size)]
-    )
+    return new_card
 
 
 print(get_mask_card_number("7000792289606361"))
 
 
-def get_mask_account(account: str) -> Any:
+def get_mask_account(account: str) -> str:
     """Функция, маскирнющая счет """
-    account_number = account.split()[-1]
 
-    private_number = (len(account_number[:-4]) * "*") + account_number[-4:]
-    chunks, chunk_size = len(private_number), len(private_number) // 4
+    account_user = str(account)
+    account = "**" + account_user[:4]
 
-    return " ".join(
-        [private_number[i: i + chunk_size] for i in range(0, chunks, chunk_size)]
-    )
-
+    return account
 
 print(get_mask_account("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,19 @@
 import masks
+def mask_account_card(nums: str) -> str:
+    """ Функция маскирует маскирует карты и счета """
+
+
+    if "Счет" in nums:
+        return masks.get_mask_account(nums)
+
+    else:
+        cards = masks.get_mask_account(nums[:-16])
+        new_card = nums.replace(nums[:-16], cards)
+        return new_card
+
+
+print(mask_account_card("Счет 35383033474447895560"))
+
+
+
 
