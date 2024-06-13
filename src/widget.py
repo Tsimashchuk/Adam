036fa9ba4246f9fbf743e843cf038be5530ee99c
Adam <adama.timosuk@gmail.com>
2024-06-14 00:47:18 +0300

    add src

--- src/widget.py
+++ src/widget.py
@@ -15,5 +15,13 @@ def mask_account_card(nums: str) -> str:
 print(mask_account_card("Счет 35383033474447895560"))
 
 
+def get_data(date: str) -> str:
+    "Функция выводящая дату"
+
+    return f"{date[9:11]}.{date[6:8]}.{date[1:5]}"
+
+print(get_data("v2018-07-11T02:26:18.671407"))
+
+
 
 
