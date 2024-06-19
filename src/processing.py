53410d602829b98fc8702e1e476beeaf0b197f73
Adam <adama.timosuk@gmail.com>
2024-06-19 22:30:32 +0300

    add processing

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,4 @@
-from typing import Any
-
-
-def filter_by_state(distionary_list: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
+def filter_by_state(distionary_list: list[dict[str, list]], state: str = "EXECUTED") -> list[dict[str, list]]:
 
     """ Функция, возвращает новый список, содержащий только те словари,
     у которых по ключу содержит переданное в функцию значение """
@@ -14,23 +11,9 @@ def filter_by_state(distionary_list: list[dict[str, Any]], state: str = "EXECUTE
     return new_list
 
 
-print(filter_by_state([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-
-
-def sort_by_date(sorted_list: list[dict[str, Any]], date: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(sorted_list: list[dict[str, list]], descendig: bool = True) -> list[dict[str, list]]:
 
     """ Функция в которой, исходные словари отсортированы по убыванию даты"""
 
-    decreasing_date = sorted(sorted_list, key=lambda t: t.get("date"), reverse=date)
+    decreasing_date = sorted(sorted_list, key=lambda t: t.get("date"), reverse=descendig)
     return decreasing_date
-
-
-print(sort_by_date([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
