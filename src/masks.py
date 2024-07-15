1e4159f3e2f1c8532a8958ec49a50468032199d2
Tsimashchuk <adama.timosuk@gmail.com>
2024-07-15 18:42:03 +0300

    Revert "fixes for dev"

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,22 @@
-def get_mask_card_number(card: str) -> str:
+from typing import Any
+
+
+def get_mask_card_number(card: Any) -> Any:
     """Функция, маскирнющая карты"""
     private_card = card[-16:-12] + " " + card[-12:-10] + "**" + " " + "****" + " " + card[-4:]
 
     return private_card
 
 
-def get_mask_account(account: str) -> str:
+print(get_mask_card_number("Maestro 1596837868705199"))
+
+
+def get_mask_account(account: Any) -> Any:
     """Функция, маскирнющая счет """
 
     account = "**" + account[-4:]
 
     return account
+
+
+print(get_mask_account("Счет 73654108430135874305"))
