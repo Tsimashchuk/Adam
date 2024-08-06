364536c954791165e974342656518d213b77a9f7
Adam <adama.timosuk@gmail.com>
2024-08-06 21:54:18 +0300

    add fuctional
    Signed-off-by: Adam <adama.timosuk@gmail.com>

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,19 @@
+import logging
+
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/masks.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card: str) -> str:
     """Функция, маскирнющая карты"""
-    private_card = card[-16:-12] + " " + card[-12:-10] + "**" + " " + "****" + " " + card[-4:]
 
+    private_card = card[-16:-12] + " " + card[-12:-10] + "**" + " " + "****" + " " + card[-4:]
+    logger.info(f'Выполняем маскировку карты')
     return private_card
 
 
@@ -9,5 +21,5 @@ def get_mask_account(account: str) -> str:
     """Функция, маскирнющая счет """
 
     account = "**" + account[-4:]
-
+    logger.info(f'Выполняем маскировку счета')
     return account
